4b428be717b6057075567df3a28229c3459c3dbe
admin <jakub.piotr.brzoska@gmail.com>
2023-07-20 23:25:56 +0200

    smakowa

---
+++ Vowel_Count/Vowel_Count.cpp
@@ -0,0 +1,46 @@
+﻿// Vowel_Count.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int getCount(const string& inputStr) { //string jest tablica znaków
+    int num_vowels = 0;
+    for (int i = 0; i < inputStr.size(); i++)
+    {
+        if (inputStr[i] == 'a'|| inputStr[i] == 'A') {//[] to indeksy stringa
+            num_vowels++;
+        }
+        if (inputStr[i] == 'e' || inputStr[i] == 'E') {
+            num_vowels++;
+        }
+        if (inputStr[i] == 'i' || inputStr[i] == 'I') {
+            num_vowels++;
+        }
+        if (inputStr[i] == 'o' || inputStr[i] == 'O') {
+            num_vowels++;
+        }
+        if (inputStr[i] == 'u' || inputStr[i] == 'U') {
+            num_vowels++;
+        }
+    }
+    return num_vowels;
+}
+
+int main()
+{
+    std::cout << getCount("Andrzej");
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
